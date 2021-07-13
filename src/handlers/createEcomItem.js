3ac0dd8cda2a939e4562dd5d5d2953989839cc27
Koushik Sadhukhan <kousadhu@publicisgroupe.net>
2021-07-13 16:18:38 +0530

    basic post request

--- src/handlers/createEcomItem.js
+++ src/handlers/createEcomItem.js
@@ -1,7 +1,26 @@
+import {v4 as uuid} from 'uuid';
+
 async function createEcomItem(event, context) {
+  const {title, description} = event.body;
+  const startDate = new Date();
+  const endDate = new Date();
+  endDate.setDate(startDate.getDay() + 1);
+
+  const auction = {
+    id: uuid(),
+    title,
+    description,
+    status: 'AVAILABLE',
+    price: {
+      amount: 0,
+      currency: 'USD'
+    },
+    createdAt: startDate.toISOString(),
+    endingAt: endDate.toISOString()
+  }
   return {
     statusCode: 200,
-    body: JSON.stringify({action: 'create ecommerce item'})
+    body: JSON.stringify({auction})
   };
 }
 
