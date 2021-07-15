52c388927892d975348127b5791dadf569858e1d
Koushik Sadhukhan <kousadhu@publicisgroupe.net>
2021-07-15 10:54:32 +0530

    DynamoDB setup

--- src/handlers/createEcomItem.js
+++ src/handlers/createEcomItem.js
@@ -1,13 +1,15 @@
 import {v4 as uuid} from 'uuid';
 import AWS from 'aws-sdk';
 
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
 async function createEcomItem(event, context) {
   const {title, description} = event.body;
   const startDate = new Date();
   const endDate = new Date();
   endDate.setDate(startDate.getDay() + 1);
 
-  const auction = {
+  const ecomItem = {
     id: uuid(),
     title,
     description,
@@ -22,12 +24,12 @@ async function createEcomItem(event, context) {
 
   await dynamoDB.put({
     TableName: process.env.ECOM_TABLE_NAME,
-    Item: auction,
+    Item: ecomItem,
   }).promise();
 
   return {
     statusCode: 200,
-    body: JSON.stringify({auction})
+    body: JSON.stringify({ecomItem})
   };
 }
 
